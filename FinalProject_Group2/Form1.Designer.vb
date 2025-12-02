c4826d07b5bc63ff5eead386b56cd73f59f28e9b
ReyAllenAntonio <ReyAllenAntonio@SMU-0121>
2025-12-02 08:39:30 +0800

    Added a clean U.I.

--- FinalProject_Group2/Form1.Designer.vb
+++ FinalProject_Group2/Form1.Designer.vb
@@ -22,10 +22,155 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.txtAuthor = New System.Windows.Forms.TextBox()
+        Me.txtCategory = New System.Windows.Forms.TextBox()
+        Me.txtAvailability = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.dgvBooks = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(45, 54)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(175, 20)
+        Me.txtID.TabIndex = 0
+        Me.txtID.Text = "ID:"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Location = New System.Drawing.Point(46, 132)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(175, 20)
+        Me.txtTitle.TabIndex = 2
+        Me.txtTitle.Text = "Title: "
+        '
+        'txtAuthor
+        '
+        Me.txtAuthor.Location = New System.Drawing.Point(46, 88)
+        Me.txtAuthor.Name = "txtAuthor"
+        Me.txtAuthor.Size = New System.Drawing.Size(175, 20)
+        Me.txtAuthor.TabIndex = 3
+        Me.txtAuthor.Text = "Author:"
+        '
+        'txtCategory
+        '
+        Me.txtCategory.Location = New System.Drawing.Point(45, 174)
+        Me.txtCategory.Name = "txtCategory"
+        Me.txtCategory.Size = New System.Drawing.Size(175, 20)
+        Me.txtCategory.TabIndex = 4
+        Me.txtCategory.Text = "Category: "
+        '
+        'txtAvailability
+        '
+        Me.txtAvailability.Location = New System.Drawing.Point(46, 208)
+        Me.txtAvailability.Name = "txtAvailability"
+        Me.txtAvailability.Size = New System.Drawing.Size(175, 20)
+        Me.txtAvailability.TabIndex = 5
+        Me.txtAvailability.Text = "Availability: "
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(45, 234)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(81, 54)
+        Me.btnAdd.TabIndex = 6
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(133, 294)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(80, 54)
+        Me.btnDelete.TabIndex = 7
+        Me.btnDelete.Text = "DELETE"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(227, 54)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(561, 54)
+        Me.btnLoad.TabIndex = 8
+        Me.btnLoad.Text = "LOAD"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(133, 234)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(80, 54)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(46, 294)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(81, 54)
+        Me.btnUpdate.TabIndex = 10
+        Me.btnUpdate.Text = "UPDATE"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'dgvBooks
+        '
+        Me.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBooks.Location = New System.Drawing.Point(227, 114)
+        Me.dgvBooks.Name = "dgvBooks"
+        Me.dgvBooks.Size = New System.Drawing.Size(561, 234)
+        Me.dgvBooks.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuBar
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgvBooks)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtAvailability)
+        Me.Controls.Add(Me.txtCategory)
+        Me.Controls.Add(Me.txtAuthor)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.txtID)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtCategory As TextBox
+    Friend WithEvents txtAvailability As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents dgvBooks As DataGridView
 End Class
